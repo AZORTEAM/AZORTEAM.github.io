c022d653b3660c55fd6acbda01f1fdf6f809cae3
AZORTEAM <57651891+AZORTEAM@users.noreply.github.com>
2020-05-30 15:12:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,109 @@
+
+(function(){
+    
+  // Inicia o firebase Firebase
+  var config = {
+  apiKey: "AIzaSyCVKx-Qz39MMyHpAqUD_f3CEGX7d4Xcy9M",
+  authDomain: "pristine-lodge-248100.firebaseapp.com",
+  databaseURL: "https://pristine-lodge-248100.firebaseio.com",
+  projectId: "pristine-lodge-248100",
+  storageBucket: "pristine-lodge-248100.appspot.com",
+  messagingSenderId: "759752779255",
+  appId: "1:759752779255:web:af8d28bea85ea508df3af2"
+  };
+  
+  firebase.initializeApp(config);
+
+  var db = firebase.database();
+
+  // Cria os listeners dos dados no firebase
+  var tempRef = db.ref('temperature');
+  var umidRef = db.ref('humidity');
+  var presenceRef = db.ref('presence');
+  var lampRef = db.ref('sala');
+
+
+  // Registra as funções que atualizam os gráficos e dados atuais da telemetria
+  tempRef.on('value', onNewData('currentTemp', 'tempLineChart' , 'Temperatura', 'C°'));
+  umidRef.on('value', onNewData('currentUmid', 'umidLineChart' , 'Umidade', '%'));
+
+
+  // Registrar função ao alterar valor de presença
+  presenceRef.on('value', function(snapshot){
+    var value = snapshot.val();
+    var el = document.getElementById('currentPresence')
+    if(value){
+      el.classList.add('green-text');
+    }else{
+      el.classList.remove('green-text');
+    }
+  });
+
+  // Registrar função ao alterar valor da lampada
+  var currentLampValue = false;
+  lampRef.on('value', function(snapshot)
+  {
+    var value = snapshot.val();
+    var el = document.getElementById('currentLamp')
+    if(value)
+	{
+      el.classList.add('amber-text');
+    }
+	else
+	{
+      el.classList.remove('amber-text');
+    }
+    currentLampValue = !!value;
+  });
+
+  // Registrar função de click no botão de lampada
+  var btnLamp = document.getElementById('btn-lamp');
+  btnLamp.addEventListener('click', function(evt)
+  {
+    lampRef.set(!currentLampValue);
+  });
+
+})();
+
+
+// Retorna uma função que de acordo com as mudanças dos dados
+// Atualiza o valor atual do elemento, com a metrica passada (currentValueEl e metric)
+// e monta o gráfico com os dados e descrição do tipo de dados (chartEl, label)
+function onNewData(currentValueEl, chartEl, label, metric){
+  return function(snapshot){
+    var readings = snapshot.val();
+    if(readings){
+        var currentValue;
+        var data = [];
+        for(var key in readings){
+          currentValue = readings[key]
+          data.push(currentValue);
+        }
+
+        document.getElementById(currentValueEl).innerText = currentValue + ' ' + metric;
+        buildLineChart(chartEl, label, data);
+    }
+  }
+}
+
+// Constroi um gráfico de linha no elemento (el) com a descrição (label) e os
+// dados passados (data)
+function buildLineChart(el, label, data){
+  var elNode = document.getElementById(el);
+  new Chart(elNode, {
+    type: 'line',
+    data: {
+        labels: new Array(data.length).fill(""),
+        datasets: [{
+            label: label,
+            data: data,
+            borderWidth: 1,
+            fill: false,
+            spanGaps: false,
+            lineTension: 0.1,
+            backgroundColor: "#F9A825",
+            borderColor: "#F9A825"
+        }]
+    }
+  });
+}
